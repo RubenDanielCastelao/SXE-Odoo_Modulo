bbdeae9cdd8e5c1350d93ad8f15b2b2946ee2637
RubenDanielCastelao <rnunezgonzalez@danielcastelao.org>
2024-03-10 18:19:06 +0100

    Añadimos el codigo para crear nuestra tabla

--- addons/openacademy/models/models.py
+++ addons/openacademy/models/models.py
@@ -16,3 +16,13 @@
 #     def _value_pc(self):
 #         for record in self:
 #             record.value2 = float(record.value) / 100
+
+from odoo import fields, models
+
+class materiales(models.Model):
+    _name = 'materiales'
+    _description = 'tabla de materiales y sus datos'
+
+    name = fields.Char(String = 'Nombre')
+    description = fields.Char(String = 'Descripcion')
+    precio = fields.Float(String = 'Precio')
